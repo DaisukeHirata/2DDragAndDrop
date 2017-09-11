6274337f165febcf677825870ab5c24dab44c2fc
Daisuke Hirata <daisukihirata@gmail.com>
2017-09-11 10:43:28 +0900

    Drag and Drop sample

---
+++ Assets/DragObject.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Image))]
+public class DragObject : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+	private Transform canvasTran;
+	private GameObject draggingObject;
+
+	void Awake()
+	{
+		canvasTran = transform.parent;
+	}
+
+	public void OnBeginDrag(PointerEventData pointerEventData)
+	{
+		CreateDragObject();
+		draggingObject.transform.position = pointerEventData.position;
+	}
+
+	public void OnDrag(PointerEventData pointerEventData)
+	{
+		draggingObject.transform.position = pointerEventData.position;
+	}
+
+	public void OnEndDrag(PointerEventData pointerEventData)
+	{
+		gameObject.GetComponent<Image>().color = Vector4.one;
+		Destroy(draggingObject);
+	}
+
+	// ドラッグオブジェクト作成
+	private void CreateDragObject()
+	{
+		draggingObject = new GameObject("Dragging Object");
+		draggingObject.transform.SetParent(canvasTran);
+		draggingObject.transform.SetAsLastSibling();
+		draggingObject.transform.localScale = Vector3.one;
+
+		// レイキャストがブロックされないように
+		CanvasGroup canvasGroup = draggingObject.AddComponent<CanvasGroup>();
+		canvasGroup.blocksRaycasts = false;
+
+		Image draggingImage = draggingObject.AddComponent<Image>();
+		Image sourceImage = GetComponent<Image>();
+
+		draggingImage.sprite = sourceImage.sprite;
+		draggingImage.rectTransform.sizeDelta = sourceImage.rectTransform.sizeDelta;
+		draggingImage.color = sourceImage.color;
+		draggingImage.material = sourceImage.material;
+
+		gameObject.GetComponent<Image>().color = Vector4.one * 0.6f;
+	}
+}
---
+++ Assets/DropObject.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class DropObject : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
+{
+    public Image image;
+	private Sprite nowSprite;
+
+	void Start()
+	{
+        image = gameObject.GetComponent<Image>();
+        nowSprite = image.sprite;
+	}
+
+	public void OnPointerEnter(PointerEventData pointerEventData)
+	{
+		if (pointerEventData.pointerDrag == null) return;
+		Image droppedImage = pointerEventData.pointerDrag.GetComponent<Image>();
+		image.sprite = droppedImage.sprite;
+		image.color = Vector4.one * 0.6f;
+	}
+
+	public void OnPointerExit(PointerEventData pointerEventData)
+	{
+		if (pointerEventData.pointerDrag == null) return;
+		image.sprite = nowSprite;
+		if (nowSprite == null)
+			image.color = Vector4.zero;
+		else
+			image.color = Vector4.one;
+	}
+
+	public void OnDrop(PointerEventData pointerEventData)
+	{
+		Image droppedImage = pointerEventData.pointerDrag.GetComponent<Image>();
+		image.sprite = droppedImage.sprite;
+		nowSprite = droppedImage.sprite;
+		image.color = Vector4.one;
+	}
+}
